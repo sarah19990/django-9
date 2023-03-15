5cfc6b8a7ef6e6fd9a7075a7a2d1e21ba2892171
sarah19990 <sara1999200407@gmail.com>
2023-03-15 17:14:49 +0000

    new changes for app services

--- itapps/settings.py
+++ itapps/settings.py
@@ -21,13 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9pldn$st!mu3u^2(6tmq7-j&e1t2yuzb6izou5k1#z8+o9w0ho'
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-9pldn$st!mu3u^2(6tmq7-j&e1t2yuzb6izou5k1#z8+o9w0ho')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('WEBSITE_HOSTNAME', 'localhost')]
 
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('WEBSITE_HOSTNAME', 'localhost')]
 
 # Application definition
 
